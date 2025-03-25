00fee5121ac8d5f712cf3c9bb9735c168bff00cd
PolinaScrbbs <polinascaraboobs@gmail.com>
2025-03-25 20:08:07 +0300

    Initial Commit

---
+++ BlazorTable/Components/Models/Element.cs
@@ -0,0 +1,25 @@
+﻿namespace BlazorTable.Components.Models
+{
+    public class ElementType
+    {
+        public string Title { get; set; }
+
+        public ElementType(string title)
+        {
+            Title = title;
+        }
+    }
+
+    public class Element
+    {
+        public string Title { get; set; }
+        public ElementType Type { get; set; }
+        public bool IsValidated { get; set; } = true;
+
+        public Element(string title, ElementType type)
+        {
+            Title = title;
+            Type = type;
+        }
+    }
+}
