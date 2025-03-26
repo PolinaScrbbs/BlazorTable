e931702b43e51ac9355d42b2f985055e73e4b3a5
PolinaScrbbs <polinascaraboobs@gmail.com>
2025-03-26 13:26:10 +0300

    Validator

--- BlazorTable/Components/Models/Element.cs
+++ BlazorTable/Components/Models/Element.cs
@@ -1,4 +1,6 @@
-﻿namespace BlazorTable.Components.Models
+﻿using BlazorTable.Components.Validators;
+
+namespace BlazorTable.Components.Models
 {
     public class ElementType
     {
@@ -14,12 +16,13 @@
     {
         public string Title { get; set; }
         public ElementType Type { get; set; }
-        public bool IsValidated { get; set; } = true;
+        public bool IsValidated { get; set; }
 
         public Element(string title, ElementType type)
         {
             Title = title;
             Type = type;
+            IsValidated = ElementValidator.Validate(title, type);
         }
     }
 }
---
+++ BlazorTable/Components/Validators/ElementValidator.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using BlazorTable.Components.Models;
+
+namespace BlazorTable.Components.Validators;
+
+public class ElementValidator
+{
+    private static readonly Regex MacAddressRegex = new(@"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", RegexOptions.Compiled);
+    
+    public static bool Validate(string value, ElementType type)
+    {
+        return type.Title switch
+        {
+            "IPv4" => ValidateIPv4(value),
+            "IPv6" => ValidateIPv6(value),
+            "Mac" => ValidateMac(value),
+            "Text" => ValidateText(value),
+            _ => false
+        };
+    }
+
+    private static bool ValidateIPv4(string value)
+    {
+        return IPAddress.TryParse(value, out var address) && address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork;
+    }
+
+    private static bool ValidateIPv6(string value)
+    {
+        return IPAddress.TryParse(value, out var address) && address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetworkV6;
+    }
+
+    private static bool ValidateMac(string value)
+    {
+        return MacAddressRegex.IsMatch(value);
+    }
+
+    private static bool ValidateText(string value)
+    {
+        return !string.IsNullOrWhiteSpace(value);
+    }
+}
